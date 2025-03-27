f40cd6195051dd46ed2fa25ba2ac96b1d2be81c9
prithvikings <prithvi07raj07@gmail.com>
2025-03-27 23:56:16 +0530

    1llm completed the basic

--- 1llm.js
+++ 1llm.js
@@ -1,14 +1,33 @@
-import "dotenv/config";
+import * as dotenv from "dotenv";
+dotenv.config();
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 
 // Text
 const model = new ChatGoogleGenerativeAI({
   model: "gemini-2.0-flash",
-    apiKey: process.env.GOOGLE_API_KEY,
+  temperature: 0.7,
   maxOutputTokens: 2048,
+  apiKey: process.env.GOOGLE_API_KEY,
+  verbose: true,
 });
 
-// Batch and stream are also supported
-const res=await model.invoke("Hello, how are you?");
+// Invoke the model mean send the request to the model
+const response = await model.invoke("Hello, how are you?");
+// console.log(response);
 
-console.log(res.content);
+// Batch and stream are also supported
+//batch basically sends multiple requests at once and returns the response in an array
+const res=await model.batch(["Hello", "how are you?", "who maded you"]);
+for (const response of res) {
+    console.log(response.content);
+}
+
+// stream sends multiple requests in a stream basically jaise jaise aata hai waise waise print hota hai
+const respo=await model.stream("Write poem about ai");
+// Print the response
+// for await (const response of respo) {
+//     console.log(response.content);
+// }
+
+
+//mainly we use invoke function to get the response
